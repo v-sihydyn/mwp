4aa9ff4a7399f9814508b8fd8216cd244969ba7a
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2022-08-25 12:36:26 +0300

    timepicker sheet

--- src/screens/RoutineRemindersScreen/RoutineRemindersScreen.tsx
+++ src/screens/RoutineRemindersScreen/RoutineRemindersScreen.tsx
@@ -1,78 +1,226 @@
-import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Text, TouchableWithoutFeedback } from 'react-native';
 import { colors } from '../../styles/colors';
 import { ReminderListItem } from './components/ReminderListItem/ReminderListItem';
+import { BottomSheet } from '../../components/BottomSheet/BottomSheet';
+import DatePicker from 'react-native-date-picker';
+import Layout from '../../constants/Layout';
+import dayjs from 'dayjs';
+import Portal from '../../components/Portal/Portal';
+import color from 'color';
+import Ripple from 'react-native-material-ripple';
 
 interface RoutineRemindersScreenProps {}
 
 type Reminder = {
+  id: number;
   routineName: string;
   time: string | null;
   repeatWeekdays: string[];
 };
 
-const reminders: Reminder[] = [
+const REMINDERS: Reminder[] = [
   {
-    routineName: '1',
+    id: 1,
+    routineName: '12',
     time: '16:03',
     repeatWeekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
   },
   {
+    id: 2,
     routineName: '2',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 3,
     routineName: '3',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 4,
     routineName: 'Pull A',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 5,
     routineName: 'Pull B',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 6,
     routineName: 'Push A',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 7,
     routineName: 'Push B',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 8,
     routineName: 'Legs',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 9,
     routineName: 'g',
     time: null,
     repeatWeekdays: [],
   },
   {
+    id: 10,
     routineName: 'hfg',
     time: null,
     repeatWeekdays: [],
   },
 ];
 
+const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 export const RoutineRemindersScreen: React.FC<RoutineRemindersScreenProps> = () => {
+  const [reminders, setReminders] = useState<Reminder[]>(REMINDERS);
+  const [draftTime, setDraftTime] = useState<Date>(new Date());
+  const [draftRepeatWeekdays, setDraftRepeatWeekdays] = useState<string[]>([]);
+  const [draftReminderIndex, setDraftReminderIndex] = useState<number | null>(null);
+  const [isSheetOpen, setSheetOpen] = useState(false);
+
+  const handleOpenSheet = ({
+    time,
+    repeatWeekdays,
+    index,
+  }: {
+    time: string | null;
+    repeatWeekdays: string[];
+    index: number;
+  }) => {
+    let _draftTime = dayjs();
+
+    if (time) {
+      const [hours, minutes] = time.split(':');
+      _draftTime = _draftTime.hour(Number(hours)).minute(Number(minutes));
+    }
+
+    setDraftTime(_draftTime.toDate());
+    setDraftRepeatWeekdays(repeatWeekdays);
+    setDraftReminderIndex(index);
+
+    setSheetOpen(true);
+  };
+
+  const handleApplyReminders = () => {
+    const _editedReminder = reminders.find((_, idx) => idx === draftReminderIndex);
+    const editedReminder = { ..._editedReminder! };
+
+    if (draftRepeatWeekdays.length > 0) {
+      editedReminder.time = dayjs(draftTime).format('HH:mm');
+    } else {
+      editedReminder.time = null;
+    }
+
+    editedReminder.repeatWeekdays = draftRepeatWeekdays;
+
+    setReminders((rms) =>
+      rms.map((r, rIdx) => {
+        return rIdx === draftReminderIndex ? editedReminder : r;
+      }),
+    );
+
+    setDraftRepeatWeekdays([]);
+    setSheetOpen(false);
+  };
+
+  const handleDeleteReminders = (index: number) => {
+    const _editedReminder = reminders.find((_, idx) => idx === index);
+    const editedReminder = { ..._editedReminder! };
+    editedReminder.time = null;
+    editedReminder.repeatWeekdays = [];
+
+    setReminders((rms) =>
+      rms.map((r, rIdx) => {
+        return rIdx === index ? editedReminder : r;
+      }),
+    );
+  };
+
+  const weekdayActiveColor = color(colors.surface2).lighten(0.24).hex();
+
   return (
     <View style={styles.container}>
       <FlatList
         data={reminders}
         renderItem={({ item, index }) => (
-          <ReminderListItem key={index} routineName={item.routineName} time={item.time} repeatWeekdays={item.repeatWeekdays} />
+          <ReminderListItem
+            key={index}
+            item={item}
+            index={index}
+            onPress={handleOpenSheet}
+            onDeleteHandlers={handleDeleteReminders}
+          />
         )}
+        keyExtractor={(item) => String(item.id)}
       />
+
+      <Portal>
+        <BottomSheet isVisible={isSheetOpen} onClose={() => setSheetOpen(false)}>
+          <View style={styles.timePickerSheet}>
+            <Text style={styles.timePickerSheetTitle}>Reminder Time</Text>
+            <View style={{ width: '100%' }}>
+              <DatePicker
+                date={draftTime}
+                onDateChange={setDraftTime}
+                androidVariant="iosClone"
+                mode="time"
+                textColor={colors.text}
+                fadeToColor="none"
+                style={{ width: Layout.window.width - 40 }}
+              />
+            </View>
+            <Text style={[styles.timePickerSheetTitle, { marginBottom: 12 }]}>Repeat Weekdays Time</Text>
+            <View style={styles.timePickerSheetWeekdays}>
+              {WEEKDAYS.map((weekday) => {
+                const isWeekdaySelected = draftRepeatWeekdays.includes(weekday);
+
+                return (
+                  <TouchableWithoutFeedback
+                    key={weekday}
+                    onPress={() => {
+                      if (!isWeekdaySelected) {
+                        setDraftRepeatWeekdays((x) => [...x, weekday]);
+                      } else {
+                        setDraftRepeatWeekdays((x) => x.filter((wd) => wd !== weekday));
+                      }
+                    }}>
+                    <View
+                      style={[
+                        styles.timePickerSheetWeekdayWrapper,
+                        isWeekdaySelected && {
+                          borderColor: colors.green,
+                          backgroundColor: weekdayActiveColor,
+                        },
+                      ]}>
+                      <Text style={[styles.timePickerSheetWeekday, isWeekdaySelected && { color: colors.text }]}>
+                        {weekday}
+                      </Text>
+                    </View>
+                  </TouchableWithoutFeedback>
+                );
+              })}
+            </View>
+            <Ripple rippleColor="#ffffff" onPress={handleApplyReminders}>
+              <View style={styles.timePickerSheetBtn}>
+                <Text style={{ color: colors.text, fontWeight: '700', fontSize: 16 }}>Apply Reminders</Text>
+              </View>
+            </Ripple>
+          </View>
+        </BottomSheet>
+      </Portal>
     </View>
   );
 };
@@ -81,9 +229,46 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.page,
     flex: 1,
-    // padding: 20,
     paddingTop: 20,
     flexDirection: 'column',
     position: 'relative',
+  },
+  weekday: {
+    marginRight: 14,
+    color: colors.text2,
+  },
+  timePickerSheet: {
+    backgroundColor: colors.surface2,
+    width: '100%',
+    padding: 20,
+  },
+  timePickerSheetTitle: {
+    color: colors.text,
+    fontSize: 18,
+  },
+  timePickerSheetWeekdays: {
+    flexDirection: 'row',
+    marginBottom: 18,
+  },
+  timePickerSheetWeekdayWrapper: {
+    marginRight: 8,
+    width: 45,
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#555',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+  },
+  timePickerSheetWeekday: {
+    color: colors.text2,
+  },
+  timePickerSheetBtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.green,
+    height: 35,
+    borderRadius: 12,
   },
 });
--- src/screens/RoutineRemindersScreen/components/ReminderListItem/ReminderListItem.tsx
+++ src/screens/RoutineRemindersScreen/components/ReminderListItem/ReminderListItem.tsx
@@ -1,123 +1,59 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Switch, TouchableWithoutFeedback, Button } from 'react-native';
-// import DateTimePicker from '@react-native-community/datetimepicker';
+import React from 'react';
+import { View, Text, StyleSheet, Switch, TouchableWithoutFeedback } from 'react-native';
 import { colors } from '../../../../styles/colors';
-import Portal from '../../../../components/Portal/Portal';
-import { BottomSheet } from '../../../../components/BottomSheet/BottomSheet';
-import Layout from '../../../../constants/Layout';
-import dayjs from 'dayjs';
 
-import DatePicker from 'react-native-date-picker'
-
-const WEEKDAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+type OnPressArgs = { time: string | null; repeatWeekdays: string[]; index: number };
 
 type RemindersListProps = {
-  routineName: string;
-  time: string | null;
-  repeatWeekdays: string[];
+  item: {
+    routineName: string;
+    time: string | null;
+    repeatWeekdays: string[];
+  };
+  index: number;
+  onPress: (args: OnPressArgs) => void;
+  onDeleteHandlers: (index: number) => void;
 };
 
-export const ReminderListItem: React.FC<RemindersListProps> = ({ routineName, time, repeatWeekdays }) => {
-  const [_time, _setTime] = useState<string | null>(time);
-  const [_repeatWeekdays, _setRepeatWeekdays] = useState(repeatWeekdays);
-  const [draftTime, setDraftTime] = useState<Date>(new Date());
-  const [draftRepeatWeekdays, setDraftRepeatWeekdays] = useState<string[]>([])
-
-  const [isSheetOpen, setSheetOpen] = useState(false);
+export const ReminderListItem: React.FC<RemindersListProps> = ({ item, index, onPress, onDeleteHandlers }) => {
+  const { time, repeatWeekdays, routineName } = item
 
   const handleRootPress = () => {
-    let _draftTime = dayjs();
-
-    if (_time) {
-      const [hours, minutes] = _time.split(':')
-      _draftTime = _draftTime.hour(Number(hours)).minute(Number(minutes));
-    }
-
-    setDraftTime(_draftTime.toDate());
-    setDraftRepeatWeekdays(_repeatWeekdays);
-
-    setSheetOpen(true)
+    onPress({ time, repeatWeekdays, index });
   };
 
   const handleRemoveReminder = () => {
-    _setTime(null);
-    _setRepeatWeekdays([])
-  }
+    onDeleteHandlers(index);
+  };
 
   return (
     <TouchableWithoutFeedback onPress={handleRootPress}>
       <View style={styles.root}>
         <View style={{ flexDirection: 'column' }}>
           <Text style={styles.title}>{routineName}</Text>
-          {_time && <Text style={styles.time}>{_time}</Text>}
-          {_repeatWeekdays.length > 0 && (
+          {time && <Text style={styles.time}>{time}</Text>}
+          {repeatWeekdays.length > 0 && (
             <View style={{ flexDirection: 'row', marginTop: 6 }}>
-              {_repeatWeekdays.map((weekday) => (
-                <Text key={weekday} style={[styles.weekday, _repeatWeekdays.includes(weekday) && { color: colors.green }]}>{weekday}</Text>
+              {repeatWeekdays.map((weekday) => (
+                <Text
+                  key={weekday}
+                  style={styles.weekday}>
+                  {weekday}
+                </Text>
               ))}
             </View>
           )}
         </View>
 
         <Switch
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={false ? '#f5dd4b' : '#f4f3f4'}
+          trackColor={{ false: '#767577', true: colors.green }}
+          thumbColor={'#f4f3f4'}
           ios_backgroundColor="#3e3e3e"
           onValueChange={() => handleRemoveReminder()}
-          value={_repeatWeekdays.length > 0}
+          value={repeatWeekdays.length > 0}
           style={{ height: 20 }}
-          disabled={_repeatWeekdays.length === 0}
+          disabled={repeatWeekdays.length === 0}
         />
-
-        {/*@TODO: move sheet to parent*/}
-        <Portal>
-          <BottomSheet isVisible={isSheetOpen} onClose={() => setSheetOpen(false)} >
-            <View style={{ backgroundColor: colors.surface2, width: '100%', }}>
-              <Text style={{ color: colors.text }}>Reminder Time</Text>
-              <View style={{ width: '100%' }}>
-                <DatePicker
-                  date={draftTime}
-                  onDateChange={setDraftTime}
-                  androidVariant="iosClone"
-                  mode="time"
-                  textColor={colors.text}
-                  fadeToColor="none"
-                  style={{ width: Layout.window.width }}
-                />
-              </View>
-
-              <Text style={{ color: colors.text }}>Repeat Weekdays Time</Text>
-              <View style={{ flexDirection: 'row', marginTop: 6 }}>
-                {WEEKDAYS.map((weekday) => (
-                  <TouchableWithoutFeedback key={weekday} onPress={() => {
-                    if (!draftRepeatWeekdays.includes(weekday)) {
-                      setDraftRepeatWeekdays(x => [...x, weekday])
-                    } else {
-                      setDraftRepeatWeekdays(x => x.filter(wd => wd !== weekday))
-                    }
-                  }}>
-                    <Text style={[styles.weekday, draftRepeatWeekdays.includes(weekday) && { color: colors.green }]}>{weekday}</Text>
-                  </TouchableWithoutFeedback>
-
-                ))}
-              </View>
-              <Button onPress={() => {
-                if (draftRepeatWeekdays.length > 0) {
-                  _setTime(dayjs(draftTime).format('HH:mm'));
-                } else {
-                  _setTime(null);
-                }
-
-                _setRepeatWeekdays(draftRepeatWeekdays);
-
-                setDraftRepeatWeekdays([]);
-                setSheetOpen(false)
-              }} title="Apply Reminders" />
-            </View>
-
-
-          </BottomSheet>
-        </Portal>
       </View>
     </TouchableWithoutFeedback>
   );

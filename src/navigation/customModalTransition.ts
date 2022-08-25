522d0db4af92f41821363951162cdb074a889b5a
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2022-08-25 14:25:13 +0300

    custom modal transition

---
+++ src/navigation/customModalTransition.ts
@@ -0,0 +1,39 @@
+import {
+  StackCardInterpolatedStyle,
+  StackCardInterpolationProps,
+  TransitionPreset,
+  HeaderStyleInterpolators,
+  TransitionSpecs,
+} from '@react-navigation/stack';
+import { Animated } from 'react-native';
+
+function forVerticalIOS({
+  current,
+  inverted,
+  layouts: { screen },
+}: StackCardInterpolationProps): StackCardInterpolatedStyle {
+  const translateY = Animated.multiply(
+    current.progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-screen.height, 0],
+      extrapolate: 'clamp',
+    }),
+    inverted
+  );
+
+  return {
+    cardStyle: {
+      transform: [{ translateY: translateY }],
+    },
+  };
+}
+
+export const ModalSlideFromTopIOS: TransitionPreset = {
+  gestureDirection: 'vertical',
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  cardStyleInterpolator: forVerticalIOS,
+  headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+};

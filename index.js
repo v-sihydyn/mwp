653b4ef486a3f3b3e15cdeb007957f563b8f1a5c
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2023-02-09 11:30:40 +0200

    setup aws project

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { AppRegistry } from 'react-native';
+import { Amplify } from 'aws-amplify';
 import App from './App';
 import { name as appName } from './app.json';
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig);
 
 AppRegistry.registerComponent(appName, () => App);

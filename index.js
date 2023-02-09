db11f800d92cd0954dd88f6fbcc2eec3c826ef87
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2023-02-09 13:44:38 +0200

    configure deep linking

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
+
 import { AppRegistry } from 'react-native';
 import { Amplify } from 'aws-amplify';
 import App from './App';

96d36838f6061e017404d612075e42bbe66ffd2c
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2023-02-09 13:02:56 +0200

    rename the app

--- android/app/src/main/java/com/vsihydyn/mwp/MainActivity.java
+++ android/app/src/main/java/com/vsihydyn/mwp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mwp2;
+package com.vsihydyn.mwp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "mwp2";
+    return "MWP";
   }
 
   /**
--- ios/MyWorkoutPlan/AppDelegate.mm
+++ ios/MyWorkoutPlan/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"mwp2", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MWP", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];

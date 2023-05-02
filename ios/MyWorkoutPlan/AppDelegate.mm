34c94aab883339474cc60489c46c6f6ef1c1aba9
Vitalii Sihydyn <vitalii.sihydyn@avenga.com>
2023-05-02 16:48:03 +0300

    wip setup firebase

--- ios/MyWorkoutPlan/AppDelegate.mm
+++ ios/MyWorkoutPlan/AppDelegate.mm
@@ -6,6 +6,7 @@
 
 #import <React/RCTAppSetupUtils.h>
 #import <React/RCTLinkingManager.h>
+#import <Firebase.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -32,6 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
